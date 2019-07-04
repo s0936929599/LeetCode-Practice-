bf3fef44d2824df6889a33204ab02d5fa3be4143
huang kuang <s0936929599@yahoo.com.tw>
2019-07-04 15:28:27 +0800

    Easy

---
+++ Python/118. Pascal's Triangle.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        ans=[]
+        if numRows==0:
+            return([])
+        else:
+            for i in range(1,numRows+1):
+                a=[]
+                if i==1:
+                    a=[1]
+                    ans.append(a)
+                elif i==2:
+                    a=[1,1]
+                    ans.append(a)
+                else:
+                    for l in range(i):  
+                        if l==0 or l==i-1:
+                            a.append(1)
+                        else:
+                            a.append(ans[i-2][l]+ans[i-2][l-1])
+                    ans.append(a)
+        return(ans)S
