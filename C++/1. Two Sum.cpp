d8ccee965860462f6f6d8ba6e60525454bb5155c
huang kuang <s0936929599@yahoo.com.tw>
2021-06-15 20:44:10 +0800

    map

---
+++ C++/1. Two Sum.cpp
@@ -0,0 +1,14 @@
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mark;
+        for(int i=0;i<nums.size();i++)
+        {
+          if(mark.find(target-nums[i])!=mark.end())
+            {
+             return {i,mark[target-nums[i]]};
+            }
+            mark[nums[i]]=i;
+        }
+        return {};
+    }
+};
