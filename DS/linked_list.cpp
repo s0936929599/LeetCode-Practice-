d12703017104f9f85df7d4a8ad557b438990ada0
huang kuang <s0936929599@yahoo.com.tw>
2021-06-18 14:55:30 +0800

    cpp

--- DS/linked_list.cpp
+++ DS/linked_list.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
  
 class Node {
@@ -43,7 +44,68 @@ void pull_back(Node *&n , int d)
      copy=copy->next;
    }
    copy->next = new_node;
+   
+   
+   //print_node(copy);
+   //cout << "..." << endl;
+  // print_node(n);
 }
+
+/*void reverse_linked( Node *& n)
+{
+    Node *  l =  n;
+   Node * tmp = new Node ();
+   Node * pre = new Node();
+  
+   print_node(l);
+   //cout << "..." << endl;
+   print_node(n);
+}*/
+
+
+void reverse_linked_vector( Node *& n)
+{
+    Node *  l =  n;
+    Node *  temp = n;
+    vector<int> v ;
+  
+  
+  while (l!=NULL)
+  {
+   v.insert(v.begin(),l->data);
+  l=l->next;
+  }
+   
+  
+  for (auto a=v.begin() ; a!= v.end();a++)
+  {
+    cout << *a << endl;
+    temp->data=*a;
+    temp=temp->next;
+  }
+ 
+  
+  
+ /* cout << "..." << endl;
+  
+   print_node(l);
+   cout << ".." << endl;
+   print_node(n);
+  */
+}
+
+void insert_position(Node * pre_node , int d)
+{
+   Node * new_node = new Node();
+   
+   new_node->data=d;
+   
+   new_node->next=pre_node->next;
+   
+   pre_node->next=new_node;
+}
+
+
 
 int main()
 {   
@@ -56,6 +118,8 @@ int main()
 
    void pull_front(Node *&, int );
    void pull_back(Node *& , int );
+   void insert_position(Node *, int);
+   void reverse_linked_vector( Node *& );
    
    head -> data = 1;
    
@@ -71,9 +135,12 @@ int main()
    
 
    // pull_front(head,10);
-   // pull_front(head,20);
+    pull_front(head,20);
     pull_back(head,40);
-    print_node(head);
+    
+    reverse_linked_vector(head);
+    //insert_position(second,15);
+    //print_node(head);
   
     return 0;
 }
