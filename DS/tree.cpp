e94b99ccccf8fdbbaaea87177afc7a477c3ddca8
huang kuang <s0936929599@yahoo.com.tw>
2021-06-24 15:51:26 +0800

    Update tree.cpp

--- DS/tree.cpp
+++ DS/tree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<queue>
 using namespace std;
 
 struct  Node 
@@ -52,23 +53,56 @@ void postorder(Node *n)
    cout<< n->data<<" ";
 }
 
-
+Node *  InsertNode(Node* root, int data)
+{
+    // If the tree is empty, assign new node address to root
+    if (root == NULL) {
+        root = CreateNode(data);
+        return root;
+    }
+ 
+    // Else, do level order traversal until we find an empty
+    // place, i.e. either left child or right child of some
+    // node is pointing to NULL.
+    queue<Node*> q;
+    q.push(root);
+ 
+    while (!q.empty()) {
+        Node* temp = q.front();
+        q.pop();
+ 
+        if (temp->left != NULL)
+            q.push(temp->left);
+        else {
+            temp->left = CreateNode(data);
+            return root;
+        }
+ 
+        if (temp->right != NULL)
+            q.push(temp->right);
+        else {
+            temp->right = CreateNode(data);
+           return root;
+        }
+    }
+}
 
 
 int main() 
 {  
 
-  /*    10
-   11           9 
-7     3    15    8
-   */ 
   Node* root = CreateNode(10);                                             
   root->left = CreateNode(11);   
-  root->left->left = CreateNode(7);
-  root->left->right = CreateNode(3);
+  //root->left->left = CreateNode(7);
+  //root->left->right = CreateNode(3);
   root->right = CreateNode(9);
-  root->right->left = CreateNode(15);
-  root->right->right = CreateNode(8);
+  //root->right->left = CreateNode(15);
+  //root->right->right = CreateNode(8);
+
+root=InsertNode(root,7);
+root=InsertNode(root,3);
+root=InsertNode(root,15);
+root=InsertNode(root,8);
 
 cout << "前序=" ;
 preorder(root);
@@ -81,10 +115,16 @@ cout << endl;
 cout << "後序=" ;
 postorder(root);
 cout << endl;
-/*
-前序=10 11 7 3 9 15 8 
+    
+  /*       10
+      11           9 
+     7     3    15     8
+                                   */ 
+    
+ /*   
+ 前序=10 11 7 3 9 15 8 
 中序=7 11 3 10 15 9 8 
-後序=7 3 11 15 8 9 10
+後序=7 3 11 15 8 9 10 
 */
   return (0);
 }
